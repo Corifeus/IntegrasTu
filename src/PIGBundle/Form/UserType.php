23a555a03fd610b6daf35e600ee2eb12f4c079f7
Fede <transylv@gmail.com>
2017-02-08 12:09:50 +0100

    Comenzamos con los usuarios

---
+++ src/PIGBundle/Form/UserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PIGBundle\Form;
+
+use PIGBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', EmailType::class)
+            ->add('email', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'first_options'  => array('label' => 'Contraseña'),
+            'second_options' => array('label' => 'Repite contraseña'),
+            ->add('submit', SubmitType::class)
+            ->add('Reset', ResetType::class)
+          ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PIGBundle\Entity\User'
+        ));
+    }
+}
