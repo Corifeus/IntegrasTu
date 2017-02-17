110efa1af26caf33aacb045eacee590ea4ba2dfe
Karshkin <karshkinstarke@gmail.com>
2017-02-17 09:30:22 +0100

    Añadidos Textarea

--- src/PIGBundle/Form/MantenimientoType.php
+++ src/PIGBundle/Form/MantenimientoType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -24,8 +25,8 @@ class MantenimientoType extends AbstractType
       $url=$_SERVER['REQUEST_URI'];
       $id=explode("/",$url);
       $builder->add('id_servicio',HiddenType::class, array('data' => $id[3]))
-        ->add('material' ,TextType::class)
-        ->add('especificaciones' ,TextType::class)
+        ->add('material' ,TextareaType::class)
+        ->add('especificaciones' ,TextareaType::class)
         ->add('Salvar',SubmitType::class)
         ->add('Borrar',ResetType::class)        ;
     }
--- src/PIGBundle/Form/OtroType.php
+++ src/PIGBundle/Form/OtroType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -24,7 +24,7 @@ class OtroType extends AbstractType
       $url=$_SERVER['REQUEST_URI'];
       $id=explode("/",$url);
       $builder->add('id_servicio',HiddenType::class, array('data' => $id[3]))
-        ->add('especificaciones' ,TextType::class)
+        ->add('especificaciones' ,TextareaType::class)
         ->add('Salvar',SubmitType::class)
         ->add('Borrar',ResetType::class)          ;
     }
