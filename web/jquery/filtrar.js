a1d2b1c97f5822ebfc409940dd641b22850e0766
Fede <transylv@gmail.com>
2017-02-20 12:34:48 +0100

    Los enlaces ya funcionan

--- web/jquery/filtrar.js
+++ web/jquery/filtrar.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $('#asignados td').each(function(i){
+    $('#asignados span').each(function(i){
         var asignados = $(this).text();
-        $('#todos td').each(function(j){
+        $('#todos span').each(function(j){
             var todos = $(this).text();
             if( todos == asignados){
-                $(this).css('display','none');
+                $(this).parent().css('display','none');
             }
         });
     });
