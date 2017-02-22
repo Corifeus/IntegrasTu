3c3a69d3f23f45718d85a5f0071411919fbe108e
Adria Chafes <Adria Chafes>
2017-02-22 12:44:21 +0100

    active y estilo modificado

--- web/js/active.js
+++ web/js/active.js
@@ -4,12 +4,11 @@ $(document).ready(function () {
         return this.href == url;
     }).parent().addClass('active');
 
-
+    var host = window.location.hostname;
     var path = window.location.pathname;
     var barra = path.split("/");
     $('ul.nav a').filter(function () {
         console.log(this.href);
-        return this.href ==('http://localhost:8000/')+ barra[1];
-        console.log(this.href);
+        return this.href =='http://'+ host + '/admin/')+ barra[2];
     }).parent().addClass('active');
 })
